5f05111bf76f8d21e72fb1c351c991ecee3bf749
newbie2525 <devidivarshithreddy@gmail.com>
2025-11-26 23:05:38 +0530

    minor change

--- src/index.js
+++ src/index.js
@@ -5,7 +5,106 @@ dotenv.config({
 // import mongoose from "mongoose";
 // import { DB_NAME } from "./constants";
 import connectDB from "./db/index.js";
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running at port :${process.env.PORT}`);
+        
+    })
+})
+.catch((err)=>{
+    console.log("MONGO DB Connection failed !!",err);
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
 import express from "express"
 const app=express()
