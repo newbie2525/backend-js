68e2a14ed365d921ee2af7db7bbe298f3f6a7701
newbie2525 <devidivarshithreddy@gmail.com>
2025-11-30 22:36:23 +0530

    add subscription model with subscriber and channel references

---
+++ src/model/subscription.model.js
@@ -0,0 +1,13 @@
+import mongoose,{Schema} from "mongoose";
+ const subscriptionSchema=new Schema({
+    subscriber:{
+        type:Schema.Types.ObjectId,//who is subscribing
+        ref:"User",
+        
+    },
+    channel:{
+        type:Schema.Types.ObjectId,//to which channel
+        ref:"User",
+    }
+ },{timewstamps:true})
+export const Subscription=mongoose.model("Subscription",subscriptionSchema)
